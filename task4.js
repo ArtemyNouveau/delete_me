b857c18576c09cc95004a09200151f9a3cef7ccb
Артем <artnovkov@gmail.com>
2020-03-22 21:18:59 +0300

    task 4 done

---
+++ task4.js
@@ -0,0 +1,13 @@
+function User(name) {
+	this.name = name;
+	this.getName = () => (this.name);
+
+	return {
+		getName: this.getName
+	}
+}
+
+const me = new User('Rex');
+
+console.log(me.getName()); // Rex
+
