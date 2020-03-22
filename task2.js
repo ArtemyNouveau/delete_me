c248ffefbe258d91cfe9190efe21f587bb2f7e27
Артем <artnovkov@gmail.com>
2020-03-22 21:06:47 +0300

    task 2 done

--- task2.js
+++ task2.js
@@ -1,7 +1,12 @@
 function sum (array) {
-    return (!Array.isArray(this) ?
+    let sum = 0;
+    if (Array.isArray(array)) sum = array.reduce((sum, num) => (sum + num));
+
+    return (
+        !Array.isArray(this) ?
         array.reduce((sum, num) => (sum + num)) :
-        this.reduce((sum, num) => (sum + num)) + array.reduce((sum, num) => (sum + num)))
+        this.reduce((sum, num) => (sum + num)) + sum
+    )
 }
 
-console.log(sum.call([5, 5, 5, 5, 5, 5, 5, 5, 5]));
+console.log(sum.call([5, 5, 5, 5, 5, 5, 5, 5, 5], [1]));
